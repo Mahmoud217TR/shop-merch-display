b575c34200f7bf444b16f6235698131739a38a5b
Mahmoud Mahmoud <54012843+Mahmoud217TR@users.noreply.github.com>
2022-07-08 03:44:32 +0300

    welcome page

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProductSearchController;
 use App\Http\Controllers\SettingsController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,15 +24,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes([
     'register' => false,
     'rest' => false,
     'verify' => false,
 ]);
+
+Route::get('/', [WelcomeController::class, 'index'])->name('welcome');
 
 Route::get('/home', [HomeController::class, 'index'])->middleware('auth')->name('home');
 
