f93194e5089fc4d2f087de2853e9b18e5e665b6e
Mahmoud Mahmoud <54012843+Mahmoud217TR@users.noreply.github.com>
2022-07-11 01:52:22 +0300

    footer ready

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,48 @@
 <footer class="container-fluid bg-dark">
     <div class="row">
-        <div class="col">
-            
+        <div class="col text-white p-5">
+            <p class="display-5 text-center pt-5">
+                {{ config('app.name', 'Laravel') }}
+            </p>
+            <ul class="nav justify-content-center flex-column flex-sm-row text-center">
+                <li class="nav-item">
+                    <a class="nav-link text-secondary" href="/">Home</a target="_blank">
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-secondary" href="#">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-secondary" href="#">Contacts</a>
+                </li>
+            </ul>
+            <ul class="nav justify-content-center  fs-1 flex-column flex-sm-row text-center">
+                <li class="nav-item">
+                    <a class="nav-link text-secondary" href="https://github.com/Mahmoud217TR" target="_blank">
+                        <i class="bi bi-github"></i>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-secondary" href="https://www.linkedin.com/in/mahmoud-mahmoud-ab8708183/" target="_blank">
+                        <i class="bi bi-linkedin"></i>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-secondary" href="https://www.facebook.com/mahmoud.mahmoud.927758/" target="_blank">
+                        <i class="bi bi-facebook"></i>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-secondary" href="https://twitter.com/Mahmoud2TR" target="_blank">
+                        <i class="bi bi-twitter"></i>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-secondary" href="https://www.youtube.com/channel/UCXUjk50xF0ePU3sn8TZAPqQ" target="_blank">
+                        <i class="bi bi-youtube"></i>
+                    </a>
+                </li>
+            </ul>
+            <p class="text-center text-secondary">Copyrights ©2022 All rights reserved | Developed by <a href="https://www.facebook.com/mahmoud.mahmoud.927758/" class="text-white text-decoration-none" target="_blank">MahmoudTR</a></p>
         </div>
     </div>
 </footer>
