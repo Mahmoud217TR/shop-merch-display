afabc4d7ed103a27aaf4be18eae0e29090312a46
Mahmoud Mahmoud <54012843+Mahmoud217TR@users.noreply.github.com>
2022-07-13 03:22:51 +0300

    added map

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -15,5 +15,10 @@
                 <img src="{{ asset('images/about.svg') }}" style="width:100%" alt="about">
             </div>
         </div>
+        <div class="row mt-3">
+            <div class="col d-flex justify-content-center">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3878368.2173869177!2d36.06658331553984!3d35.340540784645775!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1518e6dc413cc6a7%3A0x877546f4882af620!2sSyria!5e0!3m2!1sen!2s!4v1657671565199!5m2!1sen!2s" style="width:100%" height="250" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+        </div>
     </div>
 @endsection
