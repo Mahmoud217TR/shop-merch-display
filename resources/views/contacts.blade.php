a93b6a588ee27d6fa06b0468cec3a1bc37cc804b
Mahmoud Mahmoud <54012843+Mahmoud217TR@users.noreply.github.com>
2022-07-13 17:05:38 +0300

    working on contacts animation

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -4,6 +4,22 @@
 
 @section('content')
     <div class="container">
-        
+        <div class="row">
+            <div class="col">
+                <div class="contact-container dark-contact-container">
+                    <div class="contact-name">
+                        <h2>Marlo</h2>
+                    </div>
+                    <div class="contacts">
+                        <div class="contact">
+                            <p class="contact-info">0912345678</p>
+                        </div>
+                        <div class="contact">
+                            <p class="contact-info">@Someone</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
