ff731d337672a60375ff902fc5767633230792ac
Mahmoud Mahmoud <54012843+Mahmoud217TR@users.noreply.github.com>
2022-07-14 05:04:33 +0300

    new contact

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -7,8 +7,13 @@
         <div class="row">
             <div class="col d-flex justify-content-center">
                 <contact-component name='Contact Name' 
-                contact1='0912345678' contact2='@telegram'
-                contact3='@facbook' contact4='@twitter'></contact-component>
+                    contact1='0912345678' contact2='@telegram'
+                    contact3='@facbook' contact4='@twitter'>
+                </contact-component>
+                <contact-component name='Contact Name' 
+                    contact1='0912345678' contact2='@telegram'
+                    contact3='@facbook' contact4='@twitter'>
+                </contact-component>
             </div>
         </div>
     </div>
